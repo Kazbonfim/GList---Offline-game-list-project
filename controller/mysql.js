72a041107478cb5e201af57dd674823d185af0c0
Kazbonfim <zcry.games@gmail.com>
2024-10-10 02:50:28 -0300

    Iniciando projeto; estou sem sono :(

---
+++ controller/mysql.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql'); // Iniciando conexão com Mysql
+const dotenv = require('dotenv'); // Validando dados de entrada a partir de um arquivo .env
+
+dotenv.config(); // Carregando variáveis de ambiente
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+})
+
+conn.connect((error) => {
+    if (error) {
+        console.error('Erro ao conectar ao banco de dados: ', error); // Verificando erros
+        return;
+    }
+    console.log('Banco de dados conectado', conn.host); // Conectando em caso de sucesso
+})
