35e10c902d42c71a838ea1165309a4fe09278b9d
Kazbonfim <zcry.games@gmail.com>
2024-10-10 10:09:34 -0300

    Projeto de uma noite só! Ajustes na interface; possibilidade de visualizar dados inseridos no db; ajustes finos na responsividade, e novas implementações menores

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const helmet = require('helmet');
+const hbs = require('hbs');
+// Helpers
+const { getRandomColors } = require('./helpers/getRandomColors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+// Configura o diretório de views
+app.set('views', path.join(__dirname, 'views')); 
+app.use('/helpers', express.static(path.join(__dirname, 'helpers')));
+app.set('view engine', 'hbs');
+
+// Registrando Helpers
+hbs.registerHelper('getRandomColors', getRandomColors);
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,48 @@
-var express = require('express');
-var router = express.Router();
-const conn = require('../config/mysql'); // Importando conexão
+  var express = require('express');
+  var router = express.Router();
+  const conn = require('../config/mysql'); // Importando conexão
+  const hbs = require('handlebars');
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index');
-});
 
-router.post('/games/insertgame', function (req, res) {
-
-  const title = req.body.titulo;
-  const gameConsole = req.body.console;
-  const genre = req.body.genero;
-
-  const insertGame = `INSERT INTO meus_jogos (title, console, genre) VALUES (?, ?, ?);`; // Usando placeholders, para evitar SQL Injection
-
-  conn.query(insertGame, [title, gameConsole, genre], (error, results) => {
-    if (error) {
-      console.log('Houve algum erro em inserir seus dados, tente novamente: ', error);
-      return res.status(500).send('Erro ao inserir dados');
-    }
-    console.log('Dados inseridos com sucesso!', results.affectedRows);
-    res.status(201).redirect('/');
+  /* GET home page. */
+  router.get('/', function (req, res) {
+    res.render('index');
   });
 
-})
+  router.get('/games/listall', function (req, res) {
 
-module.exports = router;
+    const listAll = `SELECT * FROM meus_jogos`
+
+    conn.query(listAll, function (error, data) {
+      if (error) {
+        console.log('Houve algum erro ao recuperar seus dados ', error);
+        return res.status(500).redirect('/');
+      }
+
+      const games = data;
+
+      res.render('games', { games });
+    });
+
+  })
+
+  router.post('/games/insertgame', function (req, res) {
+
+    const title = req.body.titulo;
+    const gameConsole = req.body.console;
+    const genre = req.body.genero;
+
+    const insertGame = `INSERT INTO meus_jogos (title, console, genre) VALUES (?, ?, ?);`; // Usando placeholders, para evitar SQL Injection
+
+    conn.query(insertGame, [title, gameConsole, genre], (error, results) => {
+      if (error) {
+        console.log('Houve algum erro em inserir seus dados, tente novamente: ', error);
+        return res.status(500).send('Erro ao inserir dados');
+      }
+      console.log('Dados inseridos com sucesso!', results.affectedRows);
+      res.status(201).redirect('/');
+    });
+
+  })
+
+  module.exports = router;
